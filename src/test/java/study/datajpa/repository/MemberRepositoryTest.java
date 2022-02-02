3ca24fb002052347b0369d84792d35063adc08c8
kangyunsik <re12io@ajou.ac.kr>
2022-02-02 19:41:55 +0900

    Default Setting + jpa, db settings & check valid work

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).orElseGet(() -> new Member("NotFound"));
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        assertThat(member).isEqualTo(findMember).isEqualTo(savedMember);
+
+    }
+}
