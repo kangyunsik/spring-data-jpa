3f739a3f0648d10d96004f3f94ea829461c37205
kangyunsik <re12io@ajou.ac.kr>
2022-02-11 01:24:54 +0900

    쿼리 메소드 기능 - @EntityGraph : 간단한 경우 EntityGraph 사용. 복잡해 지는 경우 fetch join 을 사용하는 것을 권장.

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -213,4 +213,31 @@ public class MemberRepositoryTest {
         //then
         assertThat(count).isEqualTo(3);
     }
+
+    @Test
+    public void findMemberLazy(){
+        //given
+        //member1 -> TeamA
+        //member2 -> TeamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        memberRepository.save(new Member("member1", 10, teamA));
+        memberRepository.save(new Member("member1", 10, teamB));
+
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.getTeam().getClass() = " + member.getTeam().getClass());
+            System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
+        }
+    }
 }
